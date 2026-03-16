d6d3333afe410072b1c1580d67869b56941eb918
Rachit <139889495+rracchiit@users.noreply.github.com>
2026-03-16 23:12:14 +0530

    Add files via upload

---
+++ 030.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int coeff;
+    int exp;
+    struct node *next;
+};
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    struct node *head = NULL, *temp = NULL, *newnode;
+
+    // create polynomial list
+    for(int i = 0; i < n; i++)
+    {
+        newnode = (struct node*)malloc(sizeof(struct node));
+        scanf("%d %d", &newnode->coeff, &newnode->exp);
+        newnode->next = NULL;
+
+        if(head == NULL)
+            head = temp = newnode;
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    // print polynomial
+    temp = head;
+    while(temp != NULL)
+    {
+        if(temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if(temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if(temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+
+    return 0;
+}
